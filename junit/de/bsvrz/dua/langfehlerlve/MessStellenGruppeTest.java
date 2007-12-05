54bea7779b3edbd2b02aa3ab849e59d587a43c9d
bachraty <info@bitctrl.de>
2007-12-05 10:55:37 +0000

    Initial import.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.langfehlerlve@5096 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ junit/de/bsvrz/dua/langfehlerlve/MessStellenGruppeTest.java
@@ -0,0 +1,56 @@
+/**
+ * Segment 4 Datenübernahme und Aufbereitung (DUA), SWE 4.DELzFh DE Langzeit-Fehlererkennung
+ * Copyright (C) 2007 BitCtrl Systems GmbH 
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contact Information:<br>
+ * BitCtrl Systems GmbH<br>
+ * Weißenfelser Straße 67<br>
+ * 04229 Leipzig<br>
+ * Phone: +49 341-490670<br>
+ * mailto: info@bitctrl.de
+ */
+package de.bsvrz.dua.langfehlerlve;
+
+import de.bsvrz.dav.daf.main.config.SystemObject;
+import de.bsvrz.sys.funclib.bitctrl.dua.DUAInitialisierungsException;
+import de.bsvrz.sys.funclib.bitctrl.dua.lve.MessStellenGruppe;
+
+
+/**
+ * 
+ * Ermoeglicht die statische instanzen der Klasse MessStellenGruppe neustarten
+ * 
+ * @author BitCtrl Systems GmbH, Bachraty
+ *
+ */
+public class MessStellenGruppeTest extends MessStellenGruppe {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected MessStellenGruppeTest(final SystemObject msgObjekt) throws DUAInitialisierungsException {
+		super(msgObjekt);
+	}
+
+	/**
+	 * Setzt den initial-Status der Klasse
+	 */
+	public static void Reset() {
+		DAV = null;
+		SYS_OBJ_MSG_OBJ_MAP.clear();
+	}
+}
