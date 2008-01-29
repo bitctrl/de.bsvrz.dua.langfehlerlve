c0ee1dd701a4412e61b8e8b22903019a174d2089
tfelder <info@bitctrl.de>
2008-01-29 15:34:47 +0000

    KB-Inovat V.21 importiert
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.langfehlerlve@6008 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/langfehlerlve/Verbindung.java
+++ junit/de/bsvrz/dua/langfehlerlve/Verbindung.java
@@ -41,7 +41,7 @@ public class Verbindung {
 	public static final String[] CON_DATA = new String[] {
 			"-datenverteiler=localhost:8083",   //$NON-NLS-1$
 			"-benutzer=Tester",  //$NON-NLS-1$
-			"-authentifizierung=c:\\passwd1",  //$NON-NLS-1$
+			"-authentifizierung=passwd",  //$NON-NLS-1$
 			"-KonfigurationsBereichsPid=kb.deLzFhTest",  //$NON-NLS-1$
 			"-debugLevelStdErrText=INFO", //$NON-NLS-1$
 			"-debugLevelFileText=INFO" }; //$NON-NLS-1$
