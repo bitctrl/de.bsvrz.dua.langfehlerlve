cf054f661feff6f093c558f06c15e275576b0f00
tfelder <info@bitctrl.de>
2008-04-16 17:20:05 +0000

    checkstyle
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.langfehlerlve@8364 40c0be50-5d29-0410-901d-fb59edb5cafc

--- extra/skripte-bash/langfehlerlve.bash
+++ extra/skripte-bash/langfehlerlve.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# In das Verzeichnis des Skripts wechseln, damit relative Pfade funktionieren
+cd `dirname $0`
+
 source ../../../skripte-bash/einstellungen.sh
 
 ################################################################################
 # SWE-Spezifische Parameter	(überprüfen und anpassen)                          #
 ################################################################################
 
-kb="KonfigurationsBereichsPid="
+kb="kb.MQ_Konfig_A5,kb.MQ_Konfig_A6,kb.MQ_Konfig_A656,kb.MQ_Konfig_A7,kb.MQ_Konfig_A8,kb.MQ_Konfig_A81,kb.MQ_Konfig_A831,kb.MQ_Konfig_A864,kb.MQ_Konfig_A96,kb.MQ_Konfig_A98,kb.MQ_Konfig_B10,kb.MQ_Konfig_B27,kb.MQ_Konfig_DZ"
 
 ################################################################################
 # Folgende Parameter müssen überprüft und evtl. angepasst werden               #
@@ -18,6 +21,12 @@ kb="KonfigurationsBereichsPid="
 # Parameter für den Datenverteiler, als Standard werden die Einstellungen aus # einstellungen.sh verwendet.
 #dav1="-datenverteiler=localhost:8083 -benutzer=Tester -authentifizierung=passwd -debugFilePath=.."
 
+jconPort="10444"
+
+if [ "$testlauf" ]; then
+	jvmArgs=$jvmArgs" -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port="$jconPort" -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+fi
+
 ################################################################################
 # Ab hier muss nichts mehr angepasst werden                                    #
 ################################################################################
@@ -25,9 +34,10 @@ kb="KonfigurationsBereichsPid="
 # Applikation starten
 java $jvmArgs -jar ../de.bsvrz.dua.langfehlerlve-runtime.jar \
 	$dav1 \
-	$kb \
-	-debugLevelFileText=all \
-	-debugLevelStdErrText=:error \
-	-debugSetLoggerAndLevel=:none \
-	-debugSetLoggerAndLevel=de.bsvrz.iav:config \
+	-KonfigurationsBereichsPid=$kb \
+	-debugLevelStdErrText=ERROR \
+	-debugLevelFileText=INFO \
+	-debugLevelFileXML=OFF \
+	-debugLevelFileExcel=OFF \
+	-debugLevelFileHTML=OFF \
 	&
